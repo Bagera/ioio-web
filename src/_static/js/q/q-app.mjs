524c37d299b3dee41c410810093a8aa0370d121b
Victor Baquero Wihlborg <jag@vbw.se>
2020-03-13 09:01:25 +0100

    Add more rubust load of Q

--- src/_static/js/q/q-app.mjs
+++ src/_static/js/q/q-app.mjs
@@ -1,23 +1,16 @@
 import Q from "/js/q/Q.mjs";
 
-let windowLoad = false;
+let qApp;
 
 function init(onLoad) {
   const queueEl = document.querySelector(".TicketQueue");
-  if (!windowLoad) {
-    if (queueEl) {
-      const qApp = new Q(queueEl);
+  if (queueEl) {
+    if (!qApp) {
+      qApp = new Q(queueEl);
+    } else {
+      qApp.render();
     }
-  }
-  // Reset variable used to avoid double init first time
-  // This relies on turbolinks:load always being fired after
-  // window load event
-  if (onLoad) {
-    windowLoad = true;
-  } else {
-    windowLoad = false;
   }
 }
 
 document.addEventListener("turbolinks:load", () => init());
-window.addEventListener("load", () => init(true));
