b329a3f52e36f0adefb8b0a5324830fa11a46a61
Victor Baquero Wihlborg <jag@vbw.se>
2020-04-07 13:01:45 +0200

    Add QList

---
+++ src/_static/js/q/QList.mjs
@@ -0,0 +1,41 @@
+import { makeSubId } from "/js/Utils.mjs";
+
+import TicketQueue from "/js/q/TicketQueue.mjs";
+
+class QList {
+  constructor({ db, subs = [] }) {
+    this.state = {};
+    this.db = db;
+    this.subIds = {};
+
+    subs.forEach(sub => {
+      const dataType = sub.document ? sub.document : sub.collection;
+      let subId = makeSubId([sub.collection, sub.document], sub.filter);
+      this.subIds[subId] = dataType;
+
+      this.db.subscribe(sub.collection, sub.document, sub.filter);
+
+      window.addEventListener("snapshot", ({ detail }) => {
+        if (detail.subId === subId) {
+          this.setState(this.subIds[subId], detail.data);
+        }
+      });
+    });
+  }
+
+  setState(dataType, snapshotData) {
+    this.state[dataType] = snapshotData;
+    this.render();
+  }
+
+  render() {
+    const queueEl = document.querySelector(".TicketQueue");
+    const tickets = this.state.tickets;
+
+    if (queueEl) {
+      TicketQueue(queueEl, tickets, this.state.users);
+    }
+  }
+}
+
+export default QList;
---
+++ src/_static/js/q/q-list.mjs
@@ -0,0 +1,27 @@
+import QList from "/js/q/QList.mjs";
+
+let app;
+const appSelector = ".TicketQueue";
+const appSettings = {
+  db: window.db,
+  subs: [
+    { collection: "settings", document: "q" },
+    { collection: "users", document: "" },
+    { collection: "tickets", document: "", filter: ["active", "==", true] }
+  ]
+};
+
+function init() {
+  const queueEl = document.querySelector(appSelector);
+  if (queueEl) {
+    if (!app) {
+      app = new QList(appSettings);
+    } else {
+      app.render();
+    }
+  }
+}
+
+document.addEventListener("turbolinks:load", () => init());
+
+init();
