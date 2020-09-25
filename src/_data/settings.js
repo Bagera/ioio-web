4f46d65cbbfe9ddd149b60e416b7bbb960957a13
Victor Baquero Wihlborg <jag@vbw.se>
2020-09-25 12:34:24 +0200

    fixing some q stuff

--- src/_data/settings.js
+++ src/_data/settings.js
@@ -1,18 +1,19 @@
 module.exports = {
+  host: "ioio.netlify.app",
   menu: [
     { title: "Home", path: "/" },
     { title: "Lib", path: "/lib" },
     { title: "Q app", path: "/q" },
-    { title: "TAs", path: "/tas" }
+    { title: "TAs", path: "/tas" },
   ],
   social: [
     {
       title: "the ioio Facebook page",
-      path: "https://www.facebook.com/ioiomau/"
+      path: "https://www.facebook.com/ioiomau/",
     },
     {
       title: "the ioio Canvas page",
-      path: "https://mau.instructure.com/courses/5080"
-    }
-  ]
+      path: "https://mau.instructure.com/courses/5080",
+    },
+  ],
 };
